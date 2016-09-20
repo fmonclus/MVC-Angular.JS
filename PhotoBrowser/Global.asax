1645fe9eca76a598e44bde94fae643d47c30bb5d
fmonclus <fmonclus@gmail.com>
2016-09-20 14:09:59 -0300

    Add project files.

---
+++ PhotoBrowser/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhotoBrowser.MvcApplication" Language="C#" %>
---
+++ PhotoBrowser/Scripts/app.js
@@ -0,0 +1,14 @@
+﻿angular.module("photoBrowser", [])
+    .service("imageService", function ($http) {
+        return {
+            getImage: function () {
+                return $http.get("photos.json", { responseType: "json" });
+            }
+        }
+    })
+    .controller("photoList", function ($scope, imageService) {
+        $scope.message = "Hola ASP.NET MVC & Angular.JS"
+        imageService.getImage().success(function (result) {
+            $scope.images = result;
+        })
+    })
